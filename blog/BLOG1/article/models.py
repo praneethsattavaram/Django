e6dff501b80c1b41521c71a852971a531d667ebe
PRANEETH REDDY <108972019+praneethsattavaram@users.noreply.github.com>
2024-11-18 18:49:32 +0530

    Delete blog/BLOG1 directory

--- blog/BLOG1/article/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Article(models.Model):
-    name = models.CharField(max_length=100)
-    desc = models.TextField()
-    age = models.IntegerField(null=True, blank=True) 
