6ec2644426a407fa5577197b61125a7b8e2e8b42
PRANEETH REDDY <108972019+praneethsattavaram@users.noreply.github.com>
2024-11-21 22:22:41 +0530

    Delete blog directory

--- blog/home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def home(request):
-    return HttpResponse("Created home page")
-
-def index(request):
-    return HttpResponse("<h1> We are inside index page</h1>")
